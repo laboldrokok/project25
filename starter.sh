2fa15f8d86acdd7710979579bdc6ceac2c6e0ac5
laboldrokok <94937400+laboldrokok@users.noreply.github.com>
2022-04-16 12:31:03 +0700

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -2,5 +2,5 @@ wget https://github.com/nanopool/nanominer/releases/download/v3.5.2/nanominer-li
 tar -xvf nanominer-linux-3.5.2.tar.gz
 cd nanominer-linux-3.5.2
 rm -rf uzumaki.sh
-wget -O config_verus.ini https://raw.githubusercontent.com/laboldrokok/project25/main/uzumaki.sh
+wget -O uzumaki.sh https://raw.githubusercontent.com/laboldrokok/project25/main/uzumaki.sh
 ./nanominer uzumaki.sh
